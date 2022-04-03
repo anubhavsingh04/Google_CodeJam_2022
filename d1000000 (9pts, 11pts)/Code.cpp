8ed4f5271f604dbe3c561e55382046f284597258
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-04-03 16:06:19 +0530

    Add files via upload

---
+++ d1000000 (9pts, 11pts)/Code.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int t,k=1;
+    cin>>t;
+    while(t--)
+    {
+        int n,cnt=0;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0;i<n;i++)
+        cin>>v[i];
+        sort(v.begin(),v.end());
+        for(int i=0;i<n;i++)
+        {
+            if(cnt+1<=v[i])
+            cnt++;
+        }
+        cout<<"Case #"<<k<<": "<<cnt<<endl;
+        k++;
+    }
+    return(0);
+}
